fae98def2d6aa7e1810ea2077ae8606f8a07245f
ahmed okasha <ao1922302@gmail.com>
2023-11-11 02:30:06 +0200

    add base repo implementatio

---
+++ lib/features/authentication/sign/data/repository/sign_up_repo_impl.dart
@@ -0,0 +1,42 @@
+import 'package:dartz/dartz.dart';
+import 'package:intern2grow/core/error/exceptions.dart';
+import 'package:intern2grow/core/faliuer/faliuer.dart';
+import 'package:intern2grow/core/network/network_info.dart';
+import 'package:intern2grow/features/authentication/sign/data/data_source/singn_up_data_source.dart';
+import 'package:intern2grow/features/authentication/sign/data/models/user_models.dart';
+import 'package:intern2grow/features/authentication/sign/domian/repository/base_repository.dart';
+
+class SignUpRepositoryImol implements BaseAuthRepo {
+  final BaseSignUpDataSource baseSignUpDataSource;
+  final NetWorkInfo netWorkInfo;
+
+  SignUpRepositoryImol(
+      {required this.baseSignUpDataSource, required this.netWorkInfo});
+
+  @override
+  Future<Either<Failuer, UserModel>> singUp(
+      {required String userName,
+      required String emailAdress,
+      required String password}) async {
+    final response = await baseSignUpDataSource.userSignUp(
+      name: userName,
+      password: password,
+      emailAdress: emailAdress,
+    );
+    final UserModel user = UserModel.fromJson(response);
+    try {
+      return right(user);
+    } on ServerExeption catch (faliuer) {
+      return left(SereverFailuer(faliuer.errorMessageModel.statusMessage));
+    }
+  }
+
+  @override
+  Future<Either<Failuer, UserModel>> logIn(
+      {required String userName,
+      required String emailAdress,
+      required String password}) {
+    // TODO: implement logIn
+    throw UnimplementedError();
+  }
+}
